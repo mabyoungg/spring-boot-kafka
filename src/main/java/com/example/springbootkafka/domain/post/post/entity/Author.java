c1cfbfee6b4f7ddc5a5f11b49c4c8a52bdda2ae0
mabyoungg <mabyoungg@gmail.com>
2024-03-18 21:33:31 +0900

    feat: author 추가, 모듈간 종속성 제거

---
+++ src/main/java/com/example/springbootkafka/domain/post/post/entity/Author.java
@@ -0,0 +1,21 @@
+package com.example.springbootkafka.domain.post.post.entity;
+
+import com.example.springbootkafka.global.jpa.entity.BaseTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+@Getter
+@Setter
+@Table(name = "MEMBER")
+public class Author extends BaseTime {
+    @Column(name = "nickname")
+    private String writer;
+}

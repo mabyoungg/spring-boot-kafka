ca33d0a7c3972f423b63ff003a58363a3d1b8ea8
mabyoungg <mabyoungg@gmail.com>
2024-03-19 19:37:53 +0900

    fix: Post entity 타입 수정

--- src/main/java/com/example/springbootkafka/domain/post/post/entity/Post.java
+++ src/main/java/com/example/springbootkafka/domain/post/post/entity/Post.java
@@ -1,6 +1,5 @@
 package com.example.springbootkafka.domain.post.post.entity;
 
-import com.example.springbootkafka.domain.member.member.entity.Member;
 import com.example.springbootkafka.global.jpa.entity.BaseTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
@@ -16,6 +15,6 @@ import static lombok.AccessLevel.PROTECTED;
 @Setter
 public class Post extends BaseTime {
     @ManyToOne
-    private Member author;
+    private Author author;
     private String title;
 }

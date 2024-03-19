d97d68f025e6511b465b2b6f07956c1235941742
mabyoungg <mabyoungg@gmail.com>
2024-03-19 21:31:10 +0900

    refactor: 알림 등록 스프링 이벤트 적용

---
+++ src/main/java/com/example/springbootkafka/domain/noti/noti/eventListener/NotiEventListener.java
@@ -0,0 +1,20 @@
+package com.example.springbootkafka.domain.noti.noti.eventListener;
+
+import com.example.springbootkafka.domain.noti.noti.service.NotiService;
+import com.example.springbootkafka.global.event.PostCreatedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+@Transactional
+public class NotiEventListener {
+    private final NotiService notiService;
+
+    @EventListener
+    public void listen(PostCreatedEvent event) {
+        notiService.postCreated(event.getPost());
+    }
+}
--- src/main/java/com/example/springbootkafka/domain/post/post/service/PostService.java
+++ src/main/java/com/example/springbootkafka/domain/post/post/service/PostService.java
@@ -1,16 +1,15 @@
 package com.example.springbootkafka.domain.post.post.service;
 
 import com.example.springbootkafka.domain.member.member.entity.Member;
-import com.example.springbootkafka.domain.noti.noti.service.NotiService;
 import com.example.springbootkafka.domain.post.post.entity.Author;
 import com.example.springbootkafka.domain.post.post.entity.Post;
 import com.example.springbootkafka.domain.post.post.repository.PostRepository;
+import com.example.springbootkafka.global.event.PostCreatedEvent;
 import com.example.springbootkafka.global.rsData.RsData;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,11 +18,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class PostService {
     private final PostRepository postRepository;
-    @Autowired
-    @Lazy
-    private NotiService notiService;
     @PersistenceContext
     private EntityManager entityManager;
+    private final ApplicationEventPublisher publisher;
 
     @Transactional
     public RsData<Post> write(Author author, String title) {
@@ -36,13 +33,9 @@ public class PostService {
                         .build()
         );
 
-        firePostCreatedEvent(post);
+        publisher.publishEvent(new PostCreatedEvent(this, post));
 
         return RsData.of(post);
-    }
-
-    private void firePostCreatedEvent(Post post) {
-        notiService.postCreated(post);
     }
 
     public Author of(Member member) {

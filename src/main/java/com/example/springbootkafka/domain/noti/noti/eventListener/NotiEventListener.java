11c6cad7162f7b2f498e45e86ccfc518a0445ab9
mabyoungg <mabyoungg@gmail.com>
2024-03-20 23:40:02 +0900

    feat: KafkaListener 통한 topic 데이터 수신

--- src/main/java/com/example/springbootkafka/domain/noti/noti/eventListener/NotiEventListener.java
+++ src/main/java/com/example/springbootkafka/domain/noti/noti/eventListener/NotiEventListener.java
@@ -4,6 +4,7 @@ import com.example.springbootkafka.domain.noti.noti.service.NotiService;
 import com.example.springbootkafka.global.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,5 +19,10 @@ public class NotiEventListener {
     @Async
     public void listen(PostCreatedEvent event) {
         notiService.postCreated(event.getPost());
+    }
+
+    @KafkaListener(topics = "chat-room-1", groupId = "1")
+    public void consume(String message) {
+        System.out.println("message: " + message);
     }
 }

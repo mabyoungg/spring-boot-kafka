d8ba805bc5ff67b1cd89ad0158a0931f5c0c5530
mabyoungg <mabyoungg@gmail.com>
2024-03-23 19:36:07 +0900

    refactor: kafka 객체 송수신

--- src/main/java/com/example/springbootkafka/domain/noti/noti/eventListener/NotiEventListener.java
+++ src/main/java/com/example/springbootkafka/domain/noti/noti/eventListener/NotiEventListener.java
@@ -1,6 +1,7 @@
 package com.example.springbootkafka.domain.noti.noti.eventListener;
 
 import com.example.springbootkafka.domain.noti.noti.service.NotiService;
+import com.example.springbootkafka.global.dto.chat.ChatMessageDto;
 import com.example.springbootkafka.global.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
@@ -22,12 +23,12 @@ public class NotiEventListener {
     }
 
     @KafkaListener(topics = "chat-room-1", groupId = "1")
-    public void consume(String message) {
+    public void consume(ChatMessageDto message) {
         System.out.println("consume message: " + message);
     }
 
     @KafkaListener(topics = "chat-room-1", groupId = "2")
-    public void consume2(String message) {
+    public void consume2(ChatMessageDto message) {
         System.out.println("consume2 message: " + message);
     }
 }
--- src/main/java/com/example/springbootkafka/domain/post/post/service/PostService.java
+++ src/main/java/com/example/springbootkafka/domain/post/post/service/PostService.java
@@ -4,6 +4,7 @@ import com.example.springbootkafka.domain.member.member.entity.Member;
 import com.example.springbootkafka.domain.post.post.author.entity.Author;
 import com.example.springbootkafka.domain.post.post.entity.Post;
 import com.example.springbootkafka.domain.post.post.repository.PostRepository;
+import com.example.springbootkafka.global.dto.chat.ChatMessageDto;
 import com.example.springbootkafka.global.event.PostCreatedEvent;
 import com.example.springbootkafka.global.rsData.RsData;
 import jakarta.persistence.EntityManager;
@@ -36,7 +37,7 @@ public class PostService {
         );
 
         publisher.publishEvent(new PostCreatedEvent(this, post));
-        template.send("chat-room-1", post.getId() + "번 글이 생성되었습니다.");
+        template.send("chat-room-1", new ChatMessageDto(post.getId() + "번 글이 등록되었습니다."));
 
         return RsData.of(post);
     }
